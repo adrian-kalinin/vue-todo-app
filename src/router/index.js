e8813906c044ac554dcd8a51ad29aba27be0d58d
Adrian Kalinin <adriankalinin@protonmail.com>
2024-01-07 16:57:47 +0200

    Create main page for todos

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import TodosView from "@/views/TodosView.vue";
 import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: "/",
       name: "todos",
-      component: HomeView,
+      component: TodosView,
     },
     {
       path: "/about",
