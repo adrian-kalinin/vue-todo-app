3cdf4c8545befd72c2634a2e05154b86d53015e2
Adrian Kalinin <adriankalinin@protonmail.com>
2024-01-08 22:18:27 +0200

    Add validation for new todos

--- src/components/TodoCreateForm.vue
+++ src/components/TodoCreateForm.vue
@@ -1,21 +1,34 @@
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 
 const emit = defineEmits(["create-todo"]);
 
-const todo = ref("");
+const initialTodoState = {
+  todo: "",
+  valid: true,
+  error: "",
+};
+
+const todoState = reactive({ ...initialTodoState });
 
 const createTodo = () => {
-  emit("create-todo", todo.value);
-  todo.value = "";
+  if (todoState.todo === "") {
+    todoState.valid = false;
+    todoState.error = "To-do text cannot be empty.";
+    return;
+  }
+
+  emit("create-todo", todoState.todo);
+  Object.assign(todoState, initialTodoState);
 };
 </script>
 
 <template>
-  <div class="input-wrap">
-    <input type="text" v-model="todo" />
+  <div class="input-wrap" :class="{ 'input-error': !todoState.valid }">
+    <input type="text" v-model="todoState.todo" />
     <button @click="createTodo">Create</button>
   </div>
+  <p v-show="!todoState.valid" class="error-message">{{ todoState.error }}</p>
 </template>
 
 <style lang="scss" scoped>
@@ -23,6 +36,10 @@ const createTodo = () => {
   display: flex;
   transition: 250ms ease;
   border: 2px solid #41b080;
+
+  &.input-error {
+    border-color: red;
+  }
 
   &:focus-within {
     box-shadow:
@@ -44,5 +61,12 @@ const createTodo = () => {
     padding: 8px 16px;
     border: none;
   }
+}
+
+.error-message {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
