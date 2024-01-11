6742ebb2add179f30ab5eb10b389dcfbeca45a68
Adrian Kalinin <adriankalinin@protonmail.com>
2024-01-11 23:08:31 +0200

    Create component TodoItem to display todo list

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { uid } from "uid";
 import TodoCreateForm from "@/components/TodoCreateForm.vue";
+import TodoItem from "@/components/TodoItem.vue";
 
 const todoList = ref([]);
 
@@ -19,6 +20,10 @@ const createTodo = (todo) => {
   <main>
     <h1>Create To-do Task</h1>
     <TodoCreateForm @create-todo="createTodo" />
+    <ul v-if="todoList.length" class="todo-list">
+      <TodoItem v-for="todo in todoList" :key="todo.id" :todo="todo" />
+    </ul>
+    <p v-else class="todos-message">All done!</p>
   </main>
 </template>
 
@@ -34,6 +39,21 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-message {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 24px;
   }
 }
 </style>
